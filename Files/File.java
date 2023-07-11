51403ecece7a02d371e81496442fdc41093718ee
harikrishnanmkd <116252215+harikrishnanmkd@users.noreply.github.com>
2023-07-11 15:16:35 +0530

    Add files via upload

---
+++ Files/File.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class File {
+
+    public static void main(String[] args) {
+
+        try {
+            FileWriter writer = new FileWriter("File.txt",true);
+            writer.write("new file is created");
+            writer.close();
+            FileReader reader = new FileReader("File.txt");
+            BufferedReader br= new BufferedReader(reader);
+            String line;
+            System.out.println("Data read from the file:");
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+
+        } catch (IOException e) {
+            System.out.println("-----Error-----");
+        }
+
+    }
+
+}
